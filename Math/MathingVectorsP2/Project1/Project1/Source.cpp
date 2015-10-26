3281e85b54a3347c1e8666ecc32d0da0d0ec2907
atouchetAIE <tocheck3@yahoo.com>
2015-10-26 17:59:10 -0500

    Part 2

---
+++ Math/MathingVectorsP2/Project1/Project1/Source.cpp
@@ -0,0 +1,29 @@
+/*
+1)
+a 30 degrees = .524 Radians
+b 45 degrees = .785 Radians
+c 72 degrees = 1.257 Radians
+
+2)
+a 1.5 radians = 85.94 degrees
+b (3PI)/2 radians = 85.94 degrees
+c 2 radians = 114.59 degrees
+
+3)
+a DP: -3 opposite
+b DP: 0 perpendicular
+c DP: 18 positive
+d DP: 11 positive
+e DP: 0 Perpendicular
+
+4)
+a
+b 90 degrees
+c
+d 60.2 degrees
+e 90 degrees
+
+5)
+a -6, 
+
+*/
